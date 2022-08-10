f60e831f1fa9a7eb4a7b4761203462a80dc47a13
Sebrina Webster <sebrinawebster@gmail.com>
2022-08-10 11:20:58 -0400

    SQL Updates

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,14 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+
+
+insert into prize(prize_name, description, milestone, maxprizes, startdate, enddate)
+values('icecream', 'Vanilla Ice cream cone', 10,100 ,'08/09/2022', '08/30/2022' );
+
+insert into book(book_name, description, author, rating, genre)
+values('Mybook', 'Book about me', 'Me Author', 1, 'comedy');
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,69 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS prize;
 
-CREATE TABLE users (
-	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
-	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+CREATE TABLE prize (
+	prize_id SERIAL,
+	prize_name varchar(50) NOT NULL UNIQUE,
+	description varchar(50) NOT NULL,
+	milestone int NULL,
+	maxprizes int NULL,
+	startdate date NULL,
+	enddate date NULL,
+
+	CONSTRAINT PK_prizeid PRIMARY KEY (prize_id)
 );
 
+
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book (
+	book_id SERIAL,
+	book_name varchar(50) NOT NULL,
+	description varchar(50) NOT NULL,
+	author varchar(50) NOT NULL,
+	numberofpages int DEFAULT 0,
+	rating varchar(2) NOT NULL,
+	genre varchar(50) NOT NULL,
+
+	CONSTRAINT PK_bookid PRIMARY KEY (book_id)
+);
+
+DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    user_id  SERIAL,
+    username character varying(50) NOT NULL,
+    password_hash character varying(200) NOT NULL,
+    role character varying(50),
+    numofprizes integer DEFAULT 0,
+    minutesread integer DEFAULT 0,
+    booksread integer DEFAULT 0,
+    familyaccount character varying(50),
+    status smallint DEFAULT 1,
+    CONSTRAINT pk_user PRIMARY KEY (user_id),
+    CONSTRAINT users_username_key UNIQUE (username)
+);
+
+DROP TABLE IF EXISTS public.family_account;
+
+CREATE TABLE IF NOT EXISTS public.family_account
+(
+    user_id  SERIAL,
+    username character varying(50) NOT NULL,
+    password_hash character varying(200) NOT NULL,
+    role character varying(50),
+    numofprizes integer DEFAULT 0,
+    minutesread integer DEFAULT 0,
+    booksread integer DEFAULT 0,
+    familyaccount character varying(50),
+    status smallint DEFAULT 1,
+    CONSTRAINT pk_user PRIMARY KEY (user_id),
+    CONSTRAINT users_username_key UNIQUE (username)
+);
+
+
+
 COMMIT TRANSACTION;
+
