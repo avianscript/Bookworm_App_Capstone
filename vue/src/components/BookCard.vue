6dc5deef3cefcf6eb7a93c6d81d219325cb4b2d8
Davin Ferguson <davin.ferguson@icloud.com>
2022-08-15 19:24:11 -0400

    reading list on profile complete

--- vue/src/components/BookCard.vue
+++ vue/src/components/BookCard.vue
@@ -1,12 +1,19 @@
 <template>
-  <div v-on:click="selectBook()" class="card" v-bind:class="{ selected: isSelected }">
-    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
-    <h2 class="book-title">{{ book.title }}</h2>
-    <h1 class="book-author">{{ book.author }}</h1> 
+  <div class="card" v-bind:class="{ selected: isSelected }">
+    <img v-on:click="selectBook()" v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
+    <div v-bind:class="{ 'color-overlay': isSelected }"></div>
+    <p class="centered">Minutes Read:</p>
+    <form v-show="isSelected" class="centered">
+        <!-- <label for="time-read">Minutes Read:</label><br> -->
+        <input min=1 id="time-read" name="timeRead" type="number" v-model="readingActivity.minutes_read"/><br>
+        <input id="submit" v-on:click.prevent="submitReadingInfo()" type="submit"/>
+    </form>
   </div>
 </template>
 
 <script>
+import BookService from '../services/BookService';
+
 export default {
     name: 'book-card',
     props: {
@@ -15,42 +22,83 @@ export default {
     data() {
         return {
             url: "/book/" + this.book.isbn,
-            isSelected: false
+            readingActivity: {
+              username: this.$store.state.user.username, 
+              minutes_read: "",
+              isbn: this.book.isbn
+          }
         }
     },
     methods: {
         selectBook(){
-            if(this.isSelected){
-                this.isSelected = false;
-            } else {
-                this.isSelected = true;
+            if(this.$store.state.selectedBook == this.book.isbn){
+                this.$store.commit('SET_SELECTED_BOOK', "")
+            } else{
+                this.$store.commit('SET_SELECTED_BOOK', this.book.isbn)
             }
+            
+        },
+        submitReadingInfo() {
+            BookService.submitReading(this.readingActivity).then( response => {
+                if (response.status === 201) {
+                    this.$router.push('/actioncompleted')
+                }
+            })
         }
-        // viewBook(){
-        //     this.$router.push({ name: 'book-detail', params: { isbn: this.book.isbn } });
-        // },
-        // setRead(value) {
-        //     this.$store.commit('SET_READ_STATUS', {book: this.book, value: value});
-        // },
-        // addToReadingList(book) {
-        //     let addedBook = Object.assign({ read: false }, book);
-        //     delete addedBook.bestSeller;
-        //     delete addedBook.newRelease;
-        //     this.$store.commit('SAVE_BOOK', addedBook);
+        
+    },
+    computed: {
+        isSelected(){
+            return this.$store.state.selectedBook == this.book.isbn;
+        }
     }
 }
 </script>
 
 <style scoped>
+p {
+    color: white;
+    font-size: 1.1rem;
+    margin: 1px;
+}
+
+#time-read {
+    width: 5rem;
+}
+
+#submit {
+    width: 5.5rem;
+}
+
+label {
+    color: white;
+    padding-bottom: 2rem;
+
+}
+
+#time_read {
+    width: 6rem
+}
+
+.color-overlay {
+    /* width: 100%; */
+    height: 100%;
+    background: #000;
+    opacity: .7;
+    position: relative;
+    top: -101%;
+    /* z-index: 1; */
+}
+
+.centered {
+    position: relative;
+    top: -155%;
+    left: 0px;
+}
 
 .card {
-    /* border: 2px solid white; */
-    /* border-radius: 10px;
-    width: 250px;
-    height: 550px;
-    margin: 20px; */
-    padding:  1rem;
-    border-radius: 2rem;
+    height: 18rem;
+    background-color: #3f2950;
 }
 
 .card.selected {
@@ -68,6 +116,6 @@ export default {
 }
 
 img {
-    height: 20vh;
+    height: 100%;
 }
 </style>
--- vue/src/components/RecordReading.vue
+++ vue/src/components/RecordReading.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="book-container">
-    <book-card v-bind:book="book" v-for="book in $store.state.bookCompleted" v-bind:key="book.isbn" />
-  </div>
+<div>
+    <h2>Your Books:</h2>
+    <div class="book-container">
+        <book-card v-bind:book="book" v-for="book in $store.state.bookCompleted" v-bind:key="book.isbn" />
+    </div>
+</div>
+  
 </template>
 
 <script>
@@ -16,15 +20,24 @@ export default {
 </script>
 
 <style>
+h2 {
+    background-color: #3a2649;
+    color:white;
+    font-size: 2.2rem;
+    margin-bottom: 0;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
 .book-container {
-    /* display:flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap; */
+    background-color: #22162B;
     display: grid;
     gap: 1rem;
     grid-auto-flow: column;
 
     overflow-x: auto;
     overscroll-behavior-inline: contain;
+    /* border: 10px solid #57386e;
+    border-radius: 1px; */
+    height: 20rem;
 }
 </style>
